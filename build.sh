7e1c30fbf63d5d113f78505494ffde957c8fb928
qubir <support@anichips.com>
2014-01-04 14:32:35 +0800

    build.sh for phoenix

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ if [ $(basename `pwd`) != "lichee" ] ; then
 fi
 
 LICHEE_DIR=`pwd`
+UBOOT_DIR="${LICHEE_DIR}/u-boot"
 LINUX_DIR="${LICHEE_DIR}/linux-sunxi"
 OUT_DIR="${LICHEE_DIR}/out"
 PACK_DIR="${LICHEE_DIR}/tools/pack"
@@ -18,45 +19,55 @@ function pack_info()
 
 build_bootimg()
 {
-	pack_info "Build boot.img"	
-	
-	arm-linux-gnueabi-objcopy -R .note.gnu.build-id -S -O binary vmlinux bImage
-	${LICHEE_DIR}/tools/pack/pctools/linux/android/mkbootimg    \
- 		--kernel bImage \
-        	--ramdisk rootfs.cpio.gz \
-        	--board 'sun7i' \
-        	--base 0x40000000 \
-        	-o boot.img
-	
-	rm -rf bImage
-	
-	if [ ! -e boot.img ]; then
-	    echo '*build boot.img fail'
-	    exit 1
-	fi
-	mv boot.img ${OUT_DIR}/linux/common/
+    pack_info "Build boot.img"	
 
-	pack_info "Finish build boot.img"	
+    arm-linux-gnueabi-objcopy -R .note.gnu.build-id -S -O binary vmlinux bImage
+    ${LICHEE_DIR}/tools/pack/pctools/linux/android/mkbootimg    \
+	--kernel bImage \
+	--ramdisk rootfs.cpio.gz \
+	--board 'sun7i' \
+	--base 0x40000000 \
+	-o boot.img
+
+    rm -rf bImage
+
+    if [ ! -e boot.img ]; then
+        echo '*build boot.img fail'
+        exit 1
+    fi
+    mv boot.img ${OUT_DIR}/linux/common/
+
+pack_info "Finish build boot.img"	
 }
 
 build_kernel()
 {
-	pack_info "Start compile kernel"	
-	
-	make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage modules
-	
-	if [ ! -e vmlinux ]; then
-	    echo '*build Kernel fail'
-	    exit 1
-	fi
+    pack_info "Start compile kernel"	
 
-	pack_info "Finish compile kernel"
+    make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage modules
+
+    if [ ! -e vmlinux ]; then
+        echo '*build Kernel fail'
+        exit 1
+    fi
+
+    pack_info "Finish compile kernel"
+}
+
+build_uboot()
+{
+    pack_info "Start compile u-boot"
+    make distclean && make sun7i CROSS_COMPILE=arm-linux-gnueabihf-
+    pack_info "Finish compile u-boot"
 }
 
 
 if [ ! -e out ]; then
     mkdir -p out/linux/common   
 fi
+
+cd ${UBOOT_DIR}
+build_uboot
 
 cd ${LINUX_DIR}
 build_kernel
